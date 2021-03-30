d659d4fefd6a35874d37907e93a071a2b98e7160
mjustc <martajust.bcn@gmail.com>
2021-03-30 18:35:10 +0200

    add swagger dependency

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let options = {
             title: 'Swagger',
             version: '1.0.0',
         },
-        host: 'localhost:3000',
+        host: 'localhost:5000',
         basePath: '',
         produces: [
             "application/json"
